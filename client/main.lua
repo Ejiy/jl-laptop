0a2303c2a1d2b51869cd94a473030060cbf69662
FjamZoo <lauridspedersen83@gmail.com>
2022-05-21 20:19:21 +0200

    Waiting on JL SAD
    Just pushed some help functions we are gonna be needing later in the project

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local display = false
 
-function SetDisplay(bool)
+
+---- ** Globalized Varaibles if we need them other parts of the client ** ----
+
+PlayerData = {}
+CurrentCops = 0
+
+
+
+
+
+local function SetDisplay(bool)
     display = bool
     SetNuiFocus(bool, bool)
     SendNUIMessage({
@@ -10,13 +20,13 @@ function SetDisplay(bool)
     })
 end
 
-function GetApps()
+local function GetApps()
     local apps = {}
     for i = 1, #Config.Apps, 1 do
         local app = Config.Apps[i]
         if app.items then
             if type(app.items) == "table" then
-                
+
             end
         end
     end
@@ -36,12 +46,64 @@ RegisterNUICallback('close', function ()
 end)
 
 RegisterNUICallback('loadapps', function(data, cb)
-    
+
 end)
 
-CreateThread(function ()
+CreateThread(function()
     while display do
         Wait(0)
         DisableAllControlActions()
+    end
+end)
+
+
+
+---- ** Globalized functions if we need them other parts of the client ** ----
+
+-- A generic function that checks if a player has a item client side, good for faster queries for non damaging events
+function haveItem(item) -- Trigger this like if haveItem("bread") then whatever end
+    local hasItem = false
+
+    for _, v in pairs(PlayerData.items) do
+        if v.name == item then
+            hasItem = true
+            break
+        end
+    end
+
+    return hasItem
+end
+
+
+
+
+
+
+-- Handles state right when the player selects their character and location.
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+-- Resets state on logout, in case of character change.
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    PlayerData = nil
+end)
+
+-- Handles state when PlayerData is changed. We're just looking for inventory updates.
+RegisterNetEvent('QBCore:Player:SetPlayerData', function(val)
+    PlayerData = val
+end)
+
+-- Everytime a cop goes on or off duty the cop count is updated.
+RegisterNetEvent('police:SetCopCount', function(amount)
+    CurrentCops = amount
+end)
+
+-- Handles state if resource is restarted live.
+AddEventHandler('onResourceStart', function(resource)
+    if GetCurrentResourceName() == resource then
+        if LocalPlayer.state.isLoggedIn then
+            PlayerData = QBCore.Functions.GetPlayerData()
+        end
     end
 end)
