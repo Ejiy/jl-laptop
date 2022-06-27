65a9f311e78e56a33e4b625219c450ae30525299
Hyper <rjfance@gmail.com>
2022-06-27 15:07:21 -0400

    Animation for Laptop.. :)

--- client/main.lua
+++ client/main.lua
@@ -9,6 +9,12 @@ CurrentCops = 0
 
 
 
+local tabletDict = "amb@code_human_in_bus_passenger_idles@female@tablet@base"
+local tabletAnim = "base"
+local tabletProp = 'prop_cs_tablet'
+local tabletBone = 60309
+local tabletOffset = vector3(0.03, 0.002, -0.0)
+local tabletRot = vector3(10.0, 160.0, 0.0)
 
 
 local function SetDisplay(bool)
@@ -19,6 +25,8 @@ local function SetDisplay(bool)
         action = "toggle",
         status = bool,
     })
+    
+    Animation()
 end
 
 ---- ** Globalized functions if we need them other parts of the client ** ----
@@ -187,6 +195,39 @@ function CalculateTimeToDisplay()
     data.minute = minute
 
     return data
+end
+
+-- Zamn thanks ps-mdt 😎
+function Animation()
+    if not display then return end
+    -- Animation
+    RequestAnimDict(tabletDict)
+    while not HasAnimDictLoaded(tabletDict) do Citizen.Wait(100) end
+    -- Model
+    RequestModel(tabletProp)
+    while not HasModelLoaded(tabletProp) do Citizen.Wait(100) end
+
+    local plyPed = PlayerPedId()
+
+    local tabletObj = CreateObject(tabletProp, 0.0, 0.0, 0.0, true, true, false)
+
+    local tabletBoneIndex = GetPedBoneIndex(plyPed, tabletBone)
+
+    AttachEntityToEntity(tabletObj, plyPed, tabletBoneIndex, tabletOffset.x, tabletOffset.y, tabletOffset.z, tabletRot.x, tabletRot.y, tabletRot.z, true, false, false, false, 2, true)
+    SetModelAsNoLongerNeeded(tabletProp)
+
+    CreateThread(function()
+        while display do
+            Wait(0)
+            if not IsEntityPlayingAnim(plyPed, tabletDict, tabletAnim, 3) then
+                TaskPlayAnim(plyPed, tabletDict, tabletAnim, 3.0, 3.0, -1, 49, 0, 0, 0, 0)
+            end
+        end
+        ClearPedSecondaryTask(plyPed)
+        Wait(250)
+        DetachEntity(tabletObj, true, false)
+        DeleteEntity(tabletObj)
+    end)
 end
 
 CreateThread(function()
