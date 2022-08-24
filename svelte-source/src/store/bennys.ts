8a8d54785f3e04d63670fcbeab8b3faba3d65535
JustLazzy <herocraft0501@gmail.com>
2022-08-24 20:08:07 +0900

    Dummy Data

--- svelte-source/src/store/bennys.ts
+++ svelte-source/src/store/bennys.ts
@@ -1,6 +1,25 @@
 import { writable } from "svelte/store";
 import type { CartItems, Items } from "../@types/bennys";
 
-export const items = writable<Items[]>([]);
+export const items = writable<Items[]>([
+  {
+    name: "Coca-Cola",
+    price: 300,
+    image:
+      "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2020/9/16/09b4e6e5-53c9-49ca-86d3-96d7815d3f65.jpg",
+    stock: 100,
+    category: "Consumable Parts",
+    label: "Coca-Cola",
+  },
+  {
+    name: "Pepsi",
+    price: 300,
+    image:
+      "https://awsimages.detik.net.id/community/media/visual/2019/02/13/9dfb225a-637b-46e2-beb7-a31cab0553d2.jpeg",
+    stock: 100,
+    label: "Fanta",
+    category: "Consumable Parts",
+  },
+]);
 
 export const cart = writable<CartItems[]>([]);
--- svelte-source/src/store/boosting.ts
+++ svelte-source/src/store/boosting.ts
@@ -2,7 +2,48 @@ import { fetchNui } from "../utils/eventHandler";
 import { writable } from "svelte/store";
 import type { IContracts, ISaledContract } from "../@types/boosting";
 
-export const contracts = writable<IContracts[]>([]);
+export const contracts = writable<IContracts[]>([
+  {
+    id: 5,
+    contract: "S+",
+    carName: "Zentorno",
+    expire: 1660578622000,
+    cost: 1000,
+    owner: "Shitr",
+    type: "boosting",
+    vinscratch: false,
+  },
+  {
+    id: 7,
+    contract: "B",
+    carName: "Banshee",
+    expire: 1660578622000,
+    cost: 1000,
+    owner: "Fjamzoo",
+    type: "vinscratch",
+    vinscratch: true,
+  },
+  {
+    id: 6,
+    contract: "A+",
+    carName: "Sultan",
+    expire: 1660578622000,
+    cost: 1000,
+    owner: "Not Lazzy",
+    type: "vinscratch",
+    vinscratch: true,
+  },
+  {
+    id: 9,
+    contract: "D",
+    carName: "Banshee",
+    expire: 1660578622000,
+    cost: 1000,
+    owner: "Lazzy",
+    type: "boosting",
+    vinscratch: false,
+  },
+]);
 export const startedContracts = writable<IContracts>(null);
 
 let contractsStore: IContracts[];
@@ -16,16 +57,16 @@ const unsubscribeContractStart = startedContracts.subscribe((v) => {
   startedContractStore = v;
 });
 
-setInterval(() => {
-  if (contractsStore.length > 0) {
-    contractsStore.forEach((x) => {
-      if (new Date(x.expire) < new Date()) {
-        contracts.set(contractsStore.filter((y) => y.id !== x.id));
-        fetchNui("boosting/expire", { id: x.id });
-      }
-    });
-  }
-}, 1000);
+// setInterval(() => {
+//   if (contractsStore.length > 0) {
+//     contractsStore.forEach((x) => {
+//       if (new Date(x.expire) < new Date()) {
+//         contracts.set(contractsStore.filter((y) => y.id !== x.id));
+//         fetchNui("boosting/expire", { id: x.id });
+//       }
+//     });
+//   }
+// }, 1000);
 
 export function handleContracts(data: IContracts[]) {
   contracts.set(data);
